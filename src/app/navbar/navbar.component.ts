7546d03929b107a4856baf551403ab60537af382
Piotr Ferst <piotr.ferst@o2.pl>
2021-07-11 12:12:01 +0200

    stats view container added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  viewProviders: [TabsComponent]
 })
 export class NavbarComponent implements OnInit {
 
