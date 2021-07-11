e38a0462c3fd70be128f0943985eee052222a02c
Piotr Ferst <piotr.ferst@o2.pl>
2021-07-11 16:41:20 +0200

    Added interface fo links in  tabs

--- src/app/bugsRep/container/bugs-rep/bugs-rep.container.ts
+++ src/app/bugsRep/container/bugs-rep/bugs-rep.container.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BugsRepFormComponent } from '../../component/bugs-rep-form/bugs-rep-form.component';
 import { PicViewComponent } from '../../component/pic-view/pic-view.component';
+import { StatsComponent } from '../../component/stats/stats.component';
 
 @Component({
   selector: 'app-bugs-rep',
   templateUrl: './bugs-rep.container.html',
   styleUrls: ['./bugs-rep.container.scss'],
-  providers: [BugsRepFormComponent, PicViewComponent]
+  providers: [BugsRepFormComponent, PicViewComponent, StatsComponent]
 })
 export class BugsRepContainer implements OnInit {
 
