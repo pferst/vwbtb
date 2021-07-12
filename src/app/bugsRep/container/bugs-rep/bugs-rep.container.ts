e27dadab120142f4364c0cfe0bb83edddf27dcfb
Piotr Ferst <piotr.ferst@o2.pl>
2021-07-12 21:35:14 +0200

    Changes for back project to work

--- src/app/bugsRep/container/bugs-rep/bugs-rep.container.ts
+++ src/app/bugsRep/container/bugs-rep/bugs-rep.container.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BugsRepFormComponent } from '../../component/bugs-rep-form/bugs-rep-form.component';
 import { PicViewComponent } from '../../component/pic-view/pic-view.component';
 import { StatsComponent } from '../../component/stats/stats.component';
+import { MainViewContainer } from '../main-view/main-view.container';
 
 @Component({
   selector: 'app-bugs-rep',
   templateUrl: './bugs-rep.container.html',
   styleUrls: ['./bugs-rep.container.scss'],
-  providers: [BugsRepFormComponent, PicViewComponent, StatsComponent]
+  providers: [BugsRepFormComponent, PicViewComponent, StatsComponent, MainViewContainer]
 })
 export class BugsRepContainer implements OnInit {
 
--- src/app/bugsRep/container/main-view/main-view.container.ts
+++ src/app/bugsRep/container/main-view/main-view.container.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { PicViewComponent } from '../../component/pic-view/pic-view.component';
+import { StatsComponent } from '../../component/stats/stats.component';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.container.html',
-  styleUrls: ['./main-view.container.scss']
+  styleUrls: ['./main-view.container.scss'],
+  providers: [PicViewComponent, StatsComponent]
 })
 export class MainViewContainer implements OnInit {
 
