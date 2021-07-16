d1f31725831da27b0b1e1dbbfbaef4b35b707a8e
Piotr Ferst <piotr.ferst@o2.pl>
2021-07-16 14:54:00 +0200

    Form - working on

--- src/app/bugsRep/component/bugs-rep-form/bugs-rep-form.component.ts
+++ src/app/bugsRep/component/bugs-rep-form/bugs-rep-form.component.ts
@@ -4,11 +4,13 @@ import { errors } from '../../4form/errTypes';
 import { injections } from '../../4form/errInjection';
 import { ErrTypes } from '../../4form/errTypes.interface';
 import { Injection } from '../../4form/errInjection.interface';
+import { DateAdapter } from '@angular/material/core';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bugs-rep-form',
   templateUrl: './bugs-rep-form.component.html',
-  styleUrls: ['./bugs-rep-form.component.scss']
+  styleUrls: ['./bugs-rep-form.component.scss'],
 })
 export class BugsRepFormComponent implements OnInit {
 
@@ -16,18 +18,36 @@ export class BugsRepFormComponent implements OnInit {
   injectionsData: Injection[];
   form: FormGroup;
   selectedError = '';
-  constructor(private fb: FormBuilder) { }
-
+  date: Date;
+  constructor(private fb: FormBuilder, private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('en-GB');
+    this.date=new Date();
+   }
+/*   id: string,
+   date: Date,
+   procStage: string,
+   carType: string,
+   carSide: string,
+   errType: string,
+   errInclusion: string,
+   errCoordinates: Array<number> */
   ngOnInit(): void {
     this.form = this.fb.group({
       id: [null, Validators.required],
-      errType: [this.selectedError, Validators.required]
+      date: [this.date, Validators.required],
+      procStage: [null, Validators.required],
+      carType: [null, Validators.required],
+      errType: [this.selectedError, Validators.required],
+      injType: [null, Validators.required]
     });
     this.errorsData = errors;
     this.injectionsData = injections;
   }
   clear(){
 
+  }
+  today(): void{
+    this.date=new Date();
   }
 
 }
